c2dc87c50893f46aff76260161fc55887aff936b
wander-junior <wanderjrcruz96@gmail.com>
2025-07-27 14:23:18 -0300

    refactor: remove unnecessary files

--- lib/:"_elixir/library/repo".ex
+++
@@ -1,5 +0,0 @@
-defmodule :"Elixir.AlchemistLibrary.Repo" do
-  use Ecto.Repo,
-    otp_app: :alchemist_library,
-    adapter: Ecto.Adapters.Postgres
-end
--- lib/alchemist_library.ex
+++
@@ -1,2 +0,0 @@
-defmodule AlchemistLibrary do
-end
